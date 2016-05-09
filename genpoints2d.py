48f8cdd3a526af42cbf84f8dd0f8f5996bc6b015
Pavle Batuta <pavledb@gmail.com>
2016-05-09 19:39:41 +0200

    Added plotting and standard test pattern

--- genpoints2d.py
+++ genpoints2d.py
@@ -1,5 +1,7 @@
 import math
+import matplotlib.pyplot as plt
 import random
+import struct
 
 # The origin point, x = y = 0.
 ORIGIN = (0.0,0.0,0.0)
@@ -7,12 +9,25 @@ ORIGIN = (0.0,0.0,0.0)
 MAX_X_COORDINATE = 1000
 MAX_Y_COORDINATE = 1000
 
-POINTS_TO_GENERATE = 10000000
+POINTS_TO_GENERATE = 100*1000*1000
 # Indicate the number of clusters K. A value of zero or less will trigger a
 # uniform distribution.
 CLUSTER_NO = 4
 # Level of diffusion around a cluster centroid.
 DIFFUSION = 100
+
+DEFAULT_MAX_X_COORDINATE = 1000
+DEFAULT_MAX_Y_COORDINATE = 1000
+# Default centroid values:
+DEFAULT_CENTROID_LIST = [(800.0,800.0,3.0), (800.0,400.0,2.0),
+                         (400.0,800.0,1.0), (400.0,400.0,0.0)]
+DEFAULT_CLUSTER_NO = len(DEFAULT_CENTROID_LIST)
+
+DEFAULT_OUTPUT_FILE = 'test.dat'
+
+USE_DEFAULT_CENTROIDS = False
+DO_PLOT = False
+
 
 def dist2d(a, b):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
@@ -21,8 +36,8 @@ def gen_point2d(diffusion, max_x=MAX_X_COORDINATE,
                           max_y=MAX_Y_COORDINATE):
     """Generate a single centroid. A centroid may not be too close to the
     maximum value of the dimension, no closer than 2*diffusion"""
-    return (random.uniform(diffusion, max_x-2*diffusion),
-            random.uniform(diffusion, max_y-2*diffusion), 0.0)
+    return (random.uniform(2*diffusion, max_x-2*diffusion),
+            random.uniform(2*diffusion, max_y-2*diffusion), 0.0)
 
 def gen_point_from_centroid2d(centroid, diffusion):
     """Generate a single (x,y,0.0) point centered around a centroid. """
@@ -53,15 +68,20 @@ def pointgen2d(num=POINTS_TO_GENERATE, clusters=CLUSTER_NO,
     the values will be clustered into that many random groups with the
     specified level of diffusion."""
 
-    centroids = centroidgen2d(CLUSTER_NO, DIFFUSION)
+    if USE_DEFAULT_CENTROIDS:
+        centroids = DEFAULT_CENTROID_LIST
+    else:
+        centroids = centroidgen2d(CLUSTER_NO, DIFFUSION)
+
     if (clusters > 0):
-        points = [gen_point_from_centroid2d(centroids[i%clusters], DIFFUSION) 
+        points = [gen_point_from_centroid2d(
+                  centroids[i%len(centroids)], DIFFUSION) 
                   for i in xrange(num)]
     else:
         points = [gen_point2d(DIFFUSION) for i in xrange(num)]
     return (points, centroids)
 
-def main():
+def test_print():
     points, centroids = pointgen2d(num=100)
     for ci, centroid in enumerate(centroids):
         print ci, ': ', centroid
@@ -69,6 +89,21 @@ def main():
         for p in points[ci::CLUSTER_NO]:
             print p
         print '---------------------'
+
+def plot_points(points):
+    xs, ys, zs = zip(*points)
+    plt.scatter(xs, ys)
+    plt.show()
+
+def main():
+    points, centroids = pointgen2d()
+    if DO_PLOT:
+        plot_points(points)
+    out_file_name = DEFAULT_OUTPUT_FILE
+    out_file = open(out_file_name, 'wb')
+    for point in points:
+        out_file.write(struct.pack('d'*len(point), *point))
+    out_file.close()
 
 if __name__ == '__main__':
     main()
